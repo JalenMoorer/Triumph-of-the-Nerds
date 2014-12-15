1cc3fabeca6eef725ce53ac89fee846ea92383f2
jalen m moorer <jmm77@afsconnect1.njit.edu>
2014-12-15 18:50:09 -0500

    Added contact form and validation plugin

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
-
+(function($,W,D)
+{
+    var JQUERY4U = {};
+ 
+    JQUERY4U.UTIL =
+    {
+        setupFormValidation: function()
+        {
+            //form validation rules
+            $("#register-form").validate({
+                rules: {
+                    firstname: "required",
+                    lastname: "required",
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    text: {
+                        required: true,
+                        minlength: 50 //minimum characters required for the password
+                    },
+                    agree: "required"
+                },
+                messages: {
+                    firstname: "Please enter your firstname",
+                    lastname: "Please enter your lastname",
+                    text: {
+                        required: "Please provide a message",
+                        minlength: "You provide at least 50 characters"
+                    },
+                    email: "Please enter a valid email address",
+                    agree: "Please accept our policy"
+                },
+                submitHandler: function(form) {
+                    form.submit();
+                }
+            });
+        }
+    }
+ 
+    //when the dom has loaded setup form validation rules
+    $(D).ready(function($) {
+        JQUERY4U.UTIL.setupFormValidation();
+    });
+ 
+})(jQuery, window, document);
